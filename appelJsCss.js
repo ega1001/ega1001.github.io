0e6e6e6ec3910ad5777dc247e1956987d8d55e6c
ega1001 <73846786+ega1001@users.noreply.github.com>
2022-11-25 15:04:47 +0100

    Update appelJsCss.js

--- appelJsCss.js
+++ appelJsCss.js
@@ -19,19 +19,19 @@ function loadjscssfile(filename, filetype){
 }
 //loadjscssfile("https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.css", "css")
 //loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css", "css")//dynamically load and add this .css file
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/css/custom_newMoodle.css", "css") ////dynamically load and add this .css file
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/css/custom_newMoodleGrey.css", "css") ////dynamically load and add this .css file
+loadjscssfile("https://ega1001.github.io/custom_newMoodle.css", "css") ////dynamically load and add this .css file
+loadjscssfile("https://ega1001.github.io/custom_newMoodleGrey.css", "css") ////dynamically load and add this .css file
 loadjscssfile("https://www.univ-amu.fr/themes/amu_zurb/fontawesome/css/all.min.css?r5kqqk", "css") ///
 loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css")
 loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css", "css")
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/css/github.min.css", "css")
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/css/backtotop.css", "css") ////dynamically load and add this .css file
+loadjscssfile("https://ega1001.github.io/github.css", "css")
+loadjscssfile("https://ega1001.github.io/backtotop.css", "css") ////dynamically load and add this .css file
 
 
 //loadjscssfile("https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.js", "js")
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/js/uneditableIcon.js", "js")
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/js/deleteClass.js", "js") //dynamically load and add this .js file
-loadjscssfile("https://applis.inspe.univ-amu.fr/fad/js/backtotopbutton.js", "js") //dynamically load and add this .js file
+loadjscssfile("https://ega1001.github.io/uneditableIcon.js", "js")
+loadjscssfile("https://ega1001.github.io/deleteClass.js", "js") //dynamically load and add this .js file
+loadjscssfile("https://ega1001.github.io/backtotopbutton.js", "js") //dynamically load and add this .js file
 //loadjscssfile("https://applis.inspe.univ-amu.fr/fad/BackToTop/BackToTop.js", "js") //dynamically load and add this .js file
 //loadjscssfile("https://applis.inspe.univ-amu.fr/fad/js/hiddenCourse.js", "js") //dynamically load and add this .js file
 //loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js", "js") //dynamically load "javascript.php" as a JavaScript file
