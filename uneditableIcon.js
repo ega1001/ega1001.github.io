c14901f99b95d474639e0290a1110c228ec3c0e3
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-02-21 15:43:04 +0100

    maj

--- uneditableIcon.js
+++ uneditableIcon.js
@@ -1,8 +1,17 @@
 /*
 Empecher les icones d'être édités dans Atto
 */
-var uneditIcon = document.getElementsByClassName("fa");
+var classNames = ["fa", "fas", "far", "fa-solid"];
+var uneditIcon = [];
+
+for (var j=0; j<classNames.length; j++) {
+  var icons = document.getElementsByClassName(classNames[j]);
+  for (var i=0; i<icons.length; i++) {
+    uneditIcon.push(icons[i]);
+  }
+}
+
 
 for (var i=0; i<uneditIcon.length; i++) {
-  uneditIcon[i]..setAttribute("contenteditable", "false");
+  uneditIcon[i].setAttribute("contenteditable", "false");
 }
