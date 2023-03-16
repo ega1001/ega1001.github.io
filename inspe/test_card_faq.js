25f2db9bb135f08bef980770f4198ab8df63a18b
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-03-16 12:31:12 +0100

    Update test_card_faq.js

--- inspe/test_card_faq.js
+++ inspe/test_card_faq.js
@@ -1,16 +1,12 @@
 // Code jQuery à exécuter après que le document soit chargé 
 $(document).ready(function () {
-    $(".faq_card").html(`<div class="card" contenteditable = "true"> 
-    < div class = "card-header"
-    contenteditable = "true" > < /div>
-    < div class = "card-body"
-    contenteditable = "true" >
+    $(".faq_card").html(`<div class="card"> 
+    < div class = "card-header"> < /div>
+    < div class = "card-body">
     
-    < p class = "card-text"
-    contenteditable = "true" > < /p>
+    < p class = "card-text"> < /p>
     
     </div>
-    < div class = "card-footer text-muted"
-    contenteditable = "true" > < /div>
+    < div class = "card-footer text-muted"> < /div>
     </div>`); // sélectionner la div avec la classe faq et lui assigner le code HTML avec la méthode .html()
 });
