18fe3f8680cc5e8ff7b6ff3bf822474f8f7ef525
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-03-16 12:39:19 +0100

    update

--- inspe/test_card_faq.js
+++ inspe/test_card_faq.js
@@ -1,14 +1,10 @@
 // Code jQuery à exécuter après que le document soit chargé 
 $(document).ready(function () {
     $(".faq_card").html(`<div class="card"> 
-    < div class = "card-header"
-    contenteditable = "true" > < /div>
-    < div class = "card-body"
-    contenteditable = "true" >
-    
-    < p class = "card-text"> < /p>
-    
+    <div class="card-header"> </div>
+    <div class="card-body">    
+    <p class="card-text"> </p>    
     </div>
-    < div class = "card-footer text-muted"> < /div>
+    <div class="card-footer"> </div>
     </div>`); // sélectionner la div avec la classe faq et lui assigner le code HTML avec la méthode .html()
 });
