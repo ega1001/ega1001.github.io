8c781e9cb09a918d60b2c0095d5e3588d6588a4b
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-03-16 12:03:03 +0100

    Update test_card_faq.js

--- inspe/test_card_faq.js
+++ inspe/test_card_faq.js
@@ -1,12 +1,12 @@
 // Code jQuery à exécuter après que le document soit chargé 
 $(document).ready(function () {
     $(".faq_card").html(`<div class="card text-center"> 
-    <div class="card-header"> Featured </div>
+    <div class="card-header"> </div>
     <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+    
     </div>
-    <div class="card-footer text-muted"> 2 days ago </div>
+    <div class="card-footer text-muted"> </div>
     </div>`); // sélectionner la div avec la classe faq et lui assigner le code HTML avec la méthode .html()
 });
