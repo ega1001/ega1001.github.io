4bd031bfa98b4540aa13ff4b04210fe30c25bb26
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-03-21 15:55:10 +0100

    update

--- inspe/appelJsCssSandBoxOnly.js
+++ inspe/appelJsCssSandBoxOnly.js
@@ -22,7 +22,7 @@ loadjscssfile("https://www.univ-amu.fr/themes/amu_zurb/fontawesome/css/all.min.c
 loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css")
 loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css", "css")
 loadjscssfile("https://ega1001.github.io/inspe/github.min.css", "css")
-loadjscssfile("https://ega1001.github.io/inspe/test-newNav.css", "css")
+//loadjscssfile("https://ega1001.github.io/inspe/test-newNav.css", "css")//
 loadjscssfile("https://ega1001.github.io/inspe/backtotop.css", "css") ////dynamically load and add this .css file
 loadjscssfile("https://ega1001.github.io/inspe/accordionex.css", "css") ////dynamically load and add this .css file
 
@@ -30,7 +30,7 @@ loadjscssfile("https://ega1001.github.io/inspe/accordionex.css", "css") ////dyna
 //loadjscssfile("https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.js", "js")
 loadjscssfile("https://ega1001.github.io/inspe/uneditableIcon.js", "js")
 loadjscssfile("https://ega1001.github.io/inspe/deleteClassoptimized.js", "js") //dynamically load and add this .js file
-//loadjscssfile("https://ega1001.github.io/inspe/scroll-to-top.js", "js") //dynamically load and add this .js file
+loadjscssfile("https://ega1001.github.io/inspe/test_card_faq.js", "js") //dynamically load and add this .js file
 loadjscssfile("https://ega1001.github.io/inspe/backtotopbutton.js", "js") //dynamically load and add this .js file
 //loadjscssfile("https://ega1001.github.io/inspe/aos.js", "js") //dynamically load and add this .js file
 //loadjscssfile("https://ega1001.github.io/inspe/aosInit.js", "js") //dynamically load and add this .js file
