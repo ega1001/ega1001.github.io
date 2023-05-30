f56f357c8c4778c431b42c73624541b74e89c2eb
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-05-30 17:56:33 +0200

    Update resizePdf.js

--- inspe/resizePdf.js
+++ inspe/resizePdf.js
@@ -1,14 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var pdfIframe = document.getElementById('pdf-iframe');
+  var pdfUrl = 'https://applis.inspe.univ-amu.fr/fad/files/accueil/test_pdf.pdf';
+  var canvas = document.getElementById('pdf-canvas');
 
-  pdfIframe.addEventListener('load', function() {
-    pdfIframe.contentWindow.postMessage('getPDFHeight', '*');
-  });
+  // Chargement du document PDF
+  PDFJS.getDocument(pdfUrl).promise.then(function(pdfDoc) {
+    // Chargement de la première page du PDF
+    pdfDoc.getPage(1).then(function(page) {
+      var viewport = page.getViewport({ scale: 1 });
 
-  window.addEventListener('message', function(event) {
-    if (event.data && typeof event.data === 'string' && event.data.indexOf('PDFHeight:') === 0) {
-      var pdfHeight = parseInt(event.data.replace('PDFHeight:', ''), 10);
+      // Réglage de la taille du canvas pour correspondre à la taille de la page
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+
+      var renderContext = {
+        canvasContext: canvas.getContext('2d'),
+        viewport: viewport
+      };
+
+      // Affichage de la page sur le canvas
+      page.render(renderContext);
+
+      // Ajustement de la hauteur de l'iframe en fonction de la hauteur du canvas
+      var pdfHeight = canvas.height;
+      var pdfIframe = document.getElementById('pdf-iframe');
       pdfIframe.style.height = pdfHeight + 'px';
-    }
+    });
   });
 });
