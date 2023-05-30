9f2c08c1e98a09b2af091eaf24b543a4239e1bed
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-05-30 17:46:50 +0200

    Update resizePdf.js

--- inspe/resizePdf.js
+++ inspe/resizePdf.js
@@ -1,8 +1,14 @@
-window.addEventListener('DOMContentLoaded', function () {
-    var pdfIframe = document.getElementById('pdf-iframe');
-    pdfIframe.addEventListener('load', function () {
-        var pdfContent = pdfIframe.contentDocument || pdfIframe.contentWindow.document;
-        var pdfHeight = pdfContent.documentElement.scrollHeight || pdfContent.body.scrollHeight;
-        pdfIframe.style.height = pdfHeight + 'px';
-    });
+window.addEventListener('DOMContentLoaded', function() {
+  var pdfIframe = document.getElementById('pdf-iframe');
+
+  pdfIframe.addEventListener('load', function() {
+    pdfIframe.contentWindow.postMessage('getPDFHeight', '*');
+  });
+
+  window.addEventListener('message', function(event) {
+    if (event.data && typeof event.data === 'string' && event.data.indexOf('PDFHeight:') === 0) {
+      var pdfHeight = parseInt(event.data.replace('PDFHeight:', ''), 10);
+      pdfIframe.style.height = pdfHeight + 'px';
+    }
+  });
 });
