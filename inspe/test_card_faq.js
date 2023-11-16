34f407e58e1dafb29eb7bd128abde310517d30d7
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-11-16 11:53:00 +0100

    Update test_card_faq.js

--- inspe/test_card_faq.js
+++ inspe/test_card_faq.js
@@ -1,13 +1,10 @@
 // Code jQuery à exécuter après que le document soit chargé 
 (function($) {
     $(document).ready(function () {
-        $(".faq_card").wrapInner(`<div class="card"> 
-            <div class="card-header"> </div>
-            <div class="card-body">    
-                <p class="card-text"> </p>    
-            </div>
-            <div class="card-footer"> </div>
-        </div>`); // envelopper le contenu existant de la div avec la classe faq_card dans une carte
+        $(".entry").wrapInner(`<div class="card">             
+            <div class="card-body">                    
+            </div>            
+        </div>`); // envelopper le contenu existant de la div avec la classe entry dans une carte
     });
 })(jQuery);
 
