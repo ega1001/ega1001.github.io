b1e66187dff9914c2dd9241a9dcdef21861a4608
ega1001 <73846786+ega1001@users.noreply.github.com>
2023-12-01 17:40:02 +0100

    Update appelJsCss.js

--- inspe/appelJsCss.js
+++ inspe/appelJsCss.js
@@ -17,7 +17,7 @@ function loadjscssfile(filename, filetype) {
 //loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css", "css")//dynamically load and add this .css file
 loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodle.css", "css") ////dynamically load and add this .css file
 loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodleGrey.css", "css") ////dynamically load and add this .css file
-//loadjscssfile("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css", "css") ///
+loadjscssfile("https://ega1001.github.io/inspe/all.min.css", "css") ///
 //loadjscssfile("https://www.univ-amu.fr/themes/custom/amu_zurb/fontawesome/css/all.min.css?s4vlzw", "css") ///
 loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css")
 loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css", "css")
