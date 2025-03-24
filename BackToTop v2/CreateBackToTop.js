73b20e411960f8bc49e77be4ddf31971970d6ae6
ega1001 <73846786+ega1001@users.noreply.github.com>
2025-03-24 14:25:08 +0100

    Delete BackToTop v2 directory

--- BackToTop v2/CreateBackToTop.js
+++
@@ -1,9 +0,0 @@
-//create a new term
-const terms = document.createElement('a');
-
-//set id
-terms.setAttribute("id","button");
-
-//add the new element to html
-var body = document.querySelector('footer');
-body.appendChild(terms)
