5454312f67eda2cde6ba9faa345449675ceadbcf
ega1001 <73846786+ega1001@users.noreply.github.com>
2025-06-06 16:59:05 +0200

    Update appelJsCss.js

--- inspe/appelJsCss.js
+++ inspe/appelJsCss.js
@@ -20,7 +20,7 @@ function loadjscssfile(filename, filetype) {
 // Dynamically load and add CSS files
 loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodle.css", "css");
 //loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodleGrey.css", "css");
-loadjscssfile("https://ega1001.github.io/inspe/fontawesome/css/all.css", "css");
+//loadjscssfile("https://ega1001.github.io/inspe/fontawesome/css/all.css", "css");
 // loadjscssfile("https://www.univ-amu.fr/themes/custom/amu_zurb/fontawesome/css/all.min.css?s4vlzw", "css");
 loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css");
 loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css", "css");
