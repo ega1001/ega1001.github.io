133372b012c5d410de3194b11eea624feb896e24
ega1001 <73846786+ega1001@users.noreply.github.com>
2025-11-13 09:37:36 +0100

    Comment out copyCode.js loading in appelJsCss.js
    Comment out the loading of the copyCode.js file.

--- inspe/appelJsCss.js
+++ inspe/appelJsCss.js
@@ -30,4 +30,4 @@ loadjscssfile("https://ega1001.github.io/inspe/uneditableIcon.js", "js");
 loadjscssfile("https://ega1001.github.io/inspe/deleteClassoptimized.js", "js");
 loadjscssfile("https://ega1001.github.io/inspe/tutoPix.js", "js");
 //loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.js", "js");
-loadjscssfile("https://ega1001.github.io/inspe/copyCode.js", "js");
+//loadjscssfile("https://ega1001.github.io/inspe/copyCode.js", "js");
