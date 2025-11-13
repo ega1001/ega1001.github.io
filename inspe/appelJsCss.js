8017f5e98f40b29fb18f1d68f9f7acea8c674b6f
ega1001 <73846786+ega1001@users.noreply.github.com>
2025-11-13 09:21:10 +0100

    Comment out unused CSS and JS file imports
    Comment out unused CSS and JS file loads. (prismTomorrowNight css and js => enlighterjs)

--- inspe/appelJsCss.js
+++ inspe/appelJsCss.js
@@ -19,22 +19,15 @@ function loadjscssfile(filename, filetype) {
 
 // Dynamically load and add CSS files
 loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodle.css", "css");
-//loadjscssfile("https://ega1001.github.io/inspe/custom_newMoodleGrey.css", "css");
-//loadjscssfile("https://ega1001.github.io/inspe/fontawesome/css/all.css", "css");
-// loadjscssfile("https://www.univ-amu.fr/themes/custom/amu_zurb/fontawesome/css/all.min.css?s4vlzw", "css");
 loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css");
 loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css", "css");
 loadjscssfile("https://ega1001.github.io/inspe/github.min.css", "css");
 loadjscssfile("https://ega1001.github.io/inspe/backtotop.css", "css");
-loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.css", "css");
-// loadjscssfile("https://ega1001.github.io/inspe/accordionex.css", "css");
+//loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.css", "css");
 
 // Dynamically load and add JS files
 loadjscssfile("https://ega1001.github.io/inspe/uneditableIcon.js", "js");
 loadjscssfile("https://ega1001.github.io/inspe/deleteClassoptimized.js", "js");
 loadjscssfile("https://ega1001.github.io/inspe/tutoPix.js", "js");
-loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.js", "js");
+//loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.js", "js");
 loadjscssfile("https://ega1001.github.io/inspe/copyCode.js", "js");
-//loadjscssfile("https://ega1001.github.io/inspe/accordionDetailssummary.js", "js");
-// loadjscssfile("https://ega1001.github.io/inspe/BackToTop/BackToTop.js", "js");
-// loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js", "js");
