8ff3b8dac4497c9c54d37ca8002a25ad23d9f701
ega1001 <73846786+ega1001@users.noreply.github.com>
2026-06-30 17:00:46 +0200

    Add dynamic loading of JS and CSS files for Moodle 501

---
+++ inspe/appelJsCss501.js
@@ -0,0 +1,33 @@
+function loadjscssfile(filename, filetype) {
+  if (filetype === "js") {
+    if (!document.querySelector(`script[src="${filename}"]`)) {
+      var element = document.createElement("script");
+      element.setAttribute("type", "text/javascript");
+      element.setAttribute("src", filename);
+      document.body.appendChild(element);
+    }
+  } else if (filetype === "css") {
+    if (!document.querySelector(`link[href="${filename}"]`)) {
+      var element = document.createElement("link");
+      element.setAttribute("rel", "stylesheet");
+      element.setAttribute("type", "text/css");
+      element.setAttribute("href", filename);
+      document.head.appendChild(element);
+    }
+  }
+}
+
+// Dynamically load and add CSS files
+loadjscssfile("https://ega1001.github.io/inspe/custom_Moodle501.css", "css");
+loadjscssfile("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap", "css");
+loadjscssfile("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.css", "css");
+loadjscssfile("https://ega1001.github.io/inspe/github.min.css", "css");
+loadjscssfile("https://ega1001.github.io/inspe/backtotop.css", "css");
+//loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.css", "css");
+
+// Dynamically load and add JS files
+loadjscssfile("https://ega1001.github.io/inspe/uneditableIcon.js", "js");
+loadjscssfile("https://ega1001.github.io/inspe/deleteClassoptimized.js", "js");
+loadjscssfile("https://ega1001.github.io/inspe/tutoPix.js", "js");
+//loadjscssfile("https://ega1001.github.io/inspe/prism/prismTomorrowNight.js", "js");
+//loadjscssfile("https://ega1001.github.io/inspe/copyCode.js", "js");
